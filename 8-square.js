6b3688db677fe34b65308b616abbce2e892daa21
ellawendy <ellawendy13@gmail.com>
2025-07-19 00:45:40 +0300

    Fix: use uppercase X to match expected square output

--- 8-square.js
+++ 8-square.js
@@ -2,7 +2,7 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
     console.log(`Missing size`);
 } else if (size > 0) {
-    const row = `x`.repeat(size);
+    const row = `X`.repeat(size);
     for (let i = 0; i < size; i++) {
         console.log(row);
     }
